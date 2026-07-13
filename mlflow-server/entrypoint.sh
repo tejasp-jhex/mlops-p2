bf515c551d527d0b362c85a3f4d48a4ba0b3b9d4
tejasp-jhex <tejas.p@jhex.info>
2026-07-13 18:35:46 -0400

    updated the mlflow server command

--- mlflow-server/entrypoint.sh
+++ mlflow-server/entrypoint.sh
@@ -8,4 +8,6 @@ mlflow server \
     --backend-store-uri "$BACKEND_STORE_URI" \
     --default-artifact-root "$ARTIFACT_ROOT" \
     --host 0.0.0.0 \
+    --allowed-hosts "*" \
+    --cors-allowed-origins "*" \
     --port 5000
